be34dcc47949f03ac354244b2f6adcb326fc82e2
sergiojanko <janko.sergio1998@gmail.com>
2021-07-02 16:23:18 +0200

    email-validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
+/*
+
+*/
+
+// Inizializzo array
 var emails = ["pippo@gmail.com", "pluto@live.it", "paperino@hotmail.it"];
 var users = ["Pippo", "Pluto", "Paperino"];
-var username = document.getElementById("username").innerHTML
 
 console.log(emails);
 console.log(users);
+
+var username = document.getElementById("username");
+
+// Input dati
+
+var userEmail = prompt("Inserisci la tua Email", "pippo@gmail.com");
+var user;
+
+// Verifica email
+for (var i = 0; i < emails.length; i++) {
+    if (userEmail === emails[i]){
+        user = users[i];
+        console.log(userEmail)
+        console.log(user);
+    }
+}
+
+username.innerHTML = user;
+console.log(username)
+
