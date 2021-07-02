98da0becd004c6af79d0b79d40d935152d68ccba
sergiojanko <janko.sergio1998@gmail.com>
2021-07-02 16:01:38 +0200

    boilerplate

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var emails = ["pippo@gmail.com", "pluto@live.it", "paperino@hotmail.it"];
+var users = ["Pippo", "Pluto", "Paperino"];
+var username = document.getElementById("username").innerHTML
+
+console.log(emails);
+console.log(users);
